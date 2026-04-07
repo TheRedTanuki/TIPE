17d3bc696bd2032cf0c2b829662f0505512e45ec
TheRedTanuki <petitmars@outlook.com>
2026-04-07 17:58:19 +0200

    camera and voxel matching fixes

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
+#define GRAPHICS_API_OPENGL_43
 #include <raylib.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
+#include <stdint.h>
+#include <rlgl.h>
 //gcc main.c -o main -lraylib -lm -lpthread -ldl -lrt -lX11
+
+// Quaternion functions
+
+Vector3 normalizeV3(Vector3 v) {
+	double norm = sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+	return (Vector3){v.x/norm, v.y/norm, v.z/norm};
+}
+
+Quaternion normalizeQ(Quaternion q) {
+	double norm = sqrt(q.x*q.x + q.y*q.y + q.z*q.z + q.w*q.w);
+	return (Quaternion){q.x/norm, q.y/norm, q.z/norm, q.w/norm};
+}
 
 Quaternion mult(Quaternion q1, Quaternion q2) {
 	Quaternion res = {
@@ -20,16 +35,12 @@ Quaternion quatAdd(Quaternion v1, Quaternion v2) {
 	return (Quaternion) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z, v1.w + v2.w};
 }
 
-Vector3 vect3Add(Vector3 v1, Vector3 v2) {
-	return (Vector3) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+Quaternion inverse(Quaternion q) {
+	return (Quaternion){q.x, -q.y, -q.z, -q.w};
 }
 
-Vector3 scalarMultVect3(Vector3 v, double a) {
-	return (Vector3) {v.x*a, v.y*a, v.z*a};
-}
-
-Vector3 quatToVect3(Quaternion q) {
-	return (Vector3){q.y, q.z, q.w};
+Quaternion rotateQuat(Quaternion p, Quaternion q) {
+	return mult(q, mult(p, inverse(q)));
 }
 
 Quaternion rotationQuat(Quaternion axis, double angle) {
@@ -38,9 +49,18 @@ Quaternion rotationQuat(Quaternion axis, double angle) {
 	return (Quaternion){c, s*axis.y, s*axis.z, s*axis.w};
 }
 
-Quaternion inverse(Quaternion q) {
-	return (Quaternion){q.x, -q.y, -q.z, -q.w};
+Vector3 quatToVect3(Quaternion q) {
+	return (Vector3){q.y, q.z, q.w};
 }
+
+Vector3 vect3Add(Vector3 v1, Vector3 v2) {
+	return (Vector3) {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+}
+
+Vector3 scalarMultVect3(Vector3 v, double a) {
+	return (Vector3) {v.x*a, v.y*a, v.z*a};
+}
+
 
 Vector3 cross(Vector3 v1, Vector3 v2) {
 	Vector3 res = {0};
@@ -48,11 +68,6 @@ Vector3 cross(Vector3 v1, Vector3 v2) {
 	res.y = v1.z*v2.x - v1.x*v2.z;
 	res.z = v1.x*v2.y - v1.y*v2.x;
 	return res;
-}
-
-Vector3 normalize(Vector3 v) {
-	double norm = sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
-	return (Vector3){v.x/norm, v.y/norm, v.z/norm};
 }
 
 bool*** create3DBuffer(int n) {
@@ -66,14 +81,31 @@ bool*** create3DBuffer(int n) {
 	return voxelBuffer;
 }
 
+/*Image convertBoolBuffToImage(bool* buffer, int n) {
+	int size = (n*n*n-1)/32+1;
+	Image image = GenImageColor(size, 1, BLACK);
+	uint32_t bool_temp;
+	Color color_temp;
+	for(int i = 0; i<size; i++) {
+		bool_temp = 0;
+
+		int maxbits = i<(size-1) ? 32 : (32-(size*32-n*n*n));
+		for(int j = 0; j<maxbits; j++) {
+			bool_temp |= (buffer[j+i*32]? 1u : 0u)<<j;
+		}
+		memcpy(&color_temp, &bool_temp, sizeof(uint32_t));
+		ImageDrawPixel(&image, i, 0, color_temp);
+	}
+	return image;
+}*/
+
 int main ()
 {
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
+	SetConfigFlags(FLAG_WINDOW_HIGHDPI);
 	InitWindow(1280, 800, "Test");
+	
 	ToggleFullscreen();
 	float ratio = (float)GetScreenWidth()/GetScreenHeight();
-	printf("%lf\n", ratio);
 
 	Shader shader = LoadShader(0, "ressources/shader.glsl");
 	RenderTexture2D target = LoadRenderTexture(GetScreenWidth(), GetScreenHeight());
@@ -84,58 +116,53 @@ int main ()
 	int cameraRightLoc = GetShaderLocation(shader, "cameraRight");
 	int cameraUpLoc = GetShaderLocation(shader, "cameraUp");
 	int positionLoc = GetShaderLocation(shader, "position");
-	int voxelArrayLoc = GetShaderLocation(shader, "voxelArray");
 	int nLoc = GetShaderLocation(shader, "n");
+	int fovLoc = GetShaderLocation(shader, "fov");
 
 	int n = 32;
-	bool*** voxelArray = create3DBuffer(n);
-	Image img = GenImageColor(n, n*n, BLACK);
+	float fov = 1.2;
+	// int size = (n*n*n-1)/8+1;
+	uint8_t* voxelArray = calloc(n, sizeof(uint8_t));
+	int ssbo = rlLoadShaderBuffer(n, voxelArray, RL_STATIC_READ);
+	rlBindShaderBuffer(ssbo, 0);
 
-	for(int z=0; z<n; z++){
-		for(int y=0; y<n; y++){
-			for(int x=0; x<n; x++){
-				Color c = voxelArray[x][y][z] ? WHITE : BLACK;
-				ImageDrawPixel(&img, x, y + z*n, c);
-			}
-		}
-	}
-
-	Texture2D voxTex = LoadTextureFromImage(img);
-	SetTextureFilter(voxTex, TEXTURE_FILTER_POINT);
 	SetShaderValue(shader, nLoc, &n, SHADER_UNIFORM_INT);
+	SetShaderValue(shader, fovLoc, &fov, SHADER_UNIFORM_FLOAT);
 
-	Vector3 pos = {0., 0., -2.};
+	Vector3 pos = {0., 32., 0.};
 	Quaternion forward = {0., 1., 0., 0.};
 	Quaternion right = {0., 0., 1., 0.};
 	Quaternion up = {0., 0., 0., 1.};
+	
 	DisableCursor();
 
 	double pitch = 0.0;
 	double yaw = 0.0;
 	double roll = 0.0;
+	SetTargetFPS(1000);
 
 	while (!WindowShouldClose())
 	{
 		
 		Vector2 delta = GetMouseDelta();
-		
-		pitch = (double)delta.y*0.002;
-		yaw = (double)delta.x*0.002;
-		roll = 0.02*(IsKeyDown(KEY_Q)?1:0 + IsKeyDown(KEY_E)?-1:0);
+		pitch += (double)delta.y*GetFrameTime()*0.5;
+		yaw += (double)delta.x*GetFrameTime()*0.5;
+		//roll += 0.02*((IsKeyDown(KEY_Q) ? 1 : 0) + (IsKeyDown(KEY_E) ? -1 : 0));
 
-		Quaternion rQuat = mult(mult(rotationQuat(right, pitch), rotationQuat(up, yaw)), rotationQuat(forward, roll));
-		Quaternion rQuatInv = inverse(rQuat);
-		forward = mult(rQuat, mult(forward, rQuatInv));
-		right = mult(rQuat, mult(right, rQuatInv));
-		up = mult(rQuat, mult(up, rQuatInv));
+		Quaternion rQuat = mult(rotationQuat((Quaternion){0., 0., 0., 1.}, yaw), mult(rotationQuat((Quaternion){0., 0., 1., 0.}, pitch), rotationQuat((Quaternion){0., 1., 0., 0.}, roll)));
+		forward = rotateQuat((Quaternion){0., 1., 0., 0.}, rQuat);
+		right = rotateQuat((Quaternion){0., 0., 1., 0.}, rQuat);
+		up = rotateQuat((Quaternion){0., 0., 0., 1.}, rQuat);
 		Vector3 vectForward = quatToVect3(forward);
 		Vector3 vectRight = quatToVect3(right);
-		Vector3 vectUp = quatToVect3(up);;
+		Vector3 vectUp = quatToVect3(up);
 
-		if(IsKeyDown(KEY_W)) pos = vect3Add(scalarMultVect3(vectForward, 0.5), pos);
-		if(IsKeyDown(KEY_S)) pos = vect3Add(scalarMultVect3(vectForward, -0.5), pos);
-		if(IsKeyDown(KEY_A)) pos = vect3Add(scalarMultVect3(vectRight, -0.5), pos);
-		if(IsKeyDown(KEY_D)) pos = vect3Add(scalarMultVect3(vectRight, 0.5), pos);
+		if(IsKeyDown(KEY_W)) pos = vect3Add(scalarMultVect3(vectForward, GetFrameTime()*25), pos);
+		if(IsKeyDown(KEY_S)) pos = vect3Add(scalarMultVect3(vectForward, -GetFrameTime()*25), pos);
+		if(IsKeyDown(KEY_A)) pos = vect3Add(scalarMultVect3(vectRight, -GetFrameTime()*25), pos);
+		if(IsKeyDown(KEY_D)) pos = vect3Add(scalarMultVect3(vectRight, GetFrameTime()*25), pos);
+		if(IsKeyDown(KEY_SPACE)) pos = vect3Add(scalarMultVect3(vectUp, GetFrameTime()*10), pos);
+		if(IsKeyDown(KEY_LEFT_SHIFT)) pos = vect3Add(scalarMultVect3(vectUp, -GetFrameTime()*10), pos);
 		
 		SetShaderValue(shader, ratioLoc, &ratio, SHADER_UNIFORM_FLOAT);
 		SetShaderValue(shader, cameraForwardLoc, &vectForward, SHADER_UNIFORM_VEC3);
@@ -155,6 +182,7 @@ int main ()
 			EndShaderMode();
 		
 		EndDrawing();
+		printf("%d\n", GetFPS());
 	}
 
 	UnloadShader(shader);
--- ressources/shader.glsl
+++ ressources/shader.glsl
@@ -1,40 +1,47 @@
-#version 330
+#version 430
 
 in vec2 fragTexCoord;
 out vec4 finalColor;
 
 uniform float ratio;
-uniform vec3 cameraForward = vec3 (1.0, 0.0, 0.0);
-uniform vec3 cameraRight = vec3 (0.0, 1.0, 0.0);
-uniform vec3 cameraUp = vec3 (0.0, 0.0, 1.0);
-uniform vec3 position = vec3 (0.0, 0.0, -2.0);
-uniform sampler2D voxelArray;
+uniform vec3 cameraForward = vec3 (1., 0., 0.);
+uniform vec3 cameraRight = vec3 (0., 1., 0.);
+uniform vec3 cameraUp = vec3 (0., 0., 1.);
+uniform vec3 position = vec3 (0., 0., -2.0);
 uniform int n;
+uniform float fov = 1.;
 
+layout(std430, binding = 0) buffer MyBuffer {
+    float data[];
+};
 
-bool getVoxel(ivec3 p) {
-    int x = p.x;
-    int y = p.y + p.z * n;
-    vec2 uv;
-    uv.x = (float(x) + 0.5) / float(n);
-    uv.y = (float(y) + 0.5) / float(n * n);
-    return texture(voxelArray, uv).r > 0.5;
+bool outsideBox(vec3 currentVoxel) {
+    return any(lessThan(currentVoxel, vec3 (0.))) || any(greaterThan(currentVoxel, vec3 (float (n-1))));
+}
+
+bool getVoxel(vec3 currentVoxel) {
+    return  
+    // all(lessThan(currentVoxel, vec3 (30.))) && all(greaterThan(currentVoxel, vec3 (1.))); 
+    (sqrt((currentVoxel.x-16.)*(currentVoxel.x-16.) + (currentVoxel.y-16.)*(currentVoxel.y-16.) + (currentVoxel.z-16.)*(currentVoxel.z-16.)) < 10) || all(equal(currentVoxel, vec3 (31.)));
 }
 
 void main()
 {
     vec2 uv = fragTexCoord;
-    
+    // data[0] += 2; syntax example
     uv *= 2.0;
-    uv -= 1.0;
+    uv -= 1.;
     uv.x *= ratio;
 
-    vec3 ray = normalize(cameraForward + uv.x*cameraRight + uv.y*cameraUp);
+    vec3 ray = normalize(cameraForward + uv.x*cameraRight*fov + uv.y*cameraUp*fov);
+    vec3 pos = position;
+    vec3 invRay;
+    invRay.x = ray.x!=0 ? 1./ray.x : 0.;
+    invRay.y = ray.y!=0 ? 1./ray.y : 0.;
+    invRay.z = ray.z!=0 ? 1./ray.z : 0.;
 
-    vec3 invRay = 1.0 / ray;
-
-    vec3 t0 = (-position) * invRay;
-    vec3 t1 = (vec3(float(n)) - position) * invRay;
+    vec3 t0 = (vec3(0.)-pos) * invRay;
+    vec3 t1 = (vec3(float(n)) - pos) * invRay;
 
     vec3 tmin3 = min(t0, t1);
     vec3 tmax3 = max(t0, t1);
@@ -42,11 +49,51 @@ void main()
     float tmin = max(max(tmin3.x, tmin3.y), tmin3.z);
     float tmax = min(min(tmax3.x, tmax3.y), tmax3.z);
 
-    if(tmax < 0.0 || tmin > tmax){
-        finalColor = vec4(1.0, 0.0, 0.0, 1.0);
+    if(tmax < 0. || tmin > tmax){
+        finalColor = vec4(0., 0., 0., 1.);
         return;
     }
 
-    finalColor = vec4(1.0, 1.0, 1.0, 1.0);
+    if(any(lessThan(pos, vec3 (0.))) || any(greaterThan(pos, vec3 (float (n))))) {
+        pos += ray*tmin;
+    }
+
+    vec3 stepVect = sign(ray);
+    vec3 absInvRay = abs(invRay);
+    vec3 currentVoxel = floor(pos+vec3(0.0001)*stepVect);
+    
+    vec3 nextVoxelBoundary = (currentVoxel+stepVect)*1.;
+    vec3 tMax = (nextVoxelBoundary - pos)*invRay;
+
+    vec3 tDelta = 1.*stepVect*invRay;
+
+    while(true) {
+        if (outsideBox(currentVoxel)) {
+            finalColor = vec4(0., 0., 0., 1.);
+            return;
+        }
+        if (getVoxel(currentVoxel)) {
+            finalColor = vec4(currentVoxel / float(n), 1.);
+            return;
+        }
+        if (tMax.x < tMax.y) {
+            if (tMax.x < tMax.z) {
+                currentVoxel.x += stepVect.x;
+                tMax.x += tDelta.x;
+            } else {
+                currentVoxel.z += stepVect.z;
+                tMax.z += tDelta.z;
+            }
+        } else {
+            if (tMax.y < tMax.z) {
+                currentVoxel.y += stepVect.y;
+                tMax.y += tDelta.y;
+            } else {
+                currentVoxel.z += stepVect.z;
+                tMax.z += tDelta.z;
+            }
+        }
+    }
+    finalColor = vec4(0., 0., 0., 1.);
     return;
 }
