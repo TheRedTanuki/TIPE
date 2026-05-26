ef52c3bc4164b8ffc2870f6fc397e6f3fe84a741
TheRedTanuki <petitmars@outlook.com>
2026-05-26 22:24:16 +0200

    Octree creation (untested)

--- mainOctree.c
+++ mainOctree.c
@@ -8,10 +8,16 @@
 #include <stdint.h>
 #include <rlgl.h>
 #include <raymath.h>
+
+#define nullptr ((void*)0)
+
 // gcc mainOctree.c -O3 -o mainOctree -lraylib -lm -lpthread -ldl -lrt -lX11 && ./mainOctree
 
 typedef struct{
-    uint32_t data;
+    uint8_t cache;
+	uint8_t data;
+	bool isLeaf;
+	Node* children[8];
 } Node;
 
 int intClamp(int x, int m, int M) {
@@ -54,17 +60,86 @@ int quickExp(int n, int p) {
     return n*quickExp(n, p-1);
 }
 
-void computeNode(uint32_t* octtreeBuffer, int n, int nodeSize, int x, int y, int z, int index) {
+uint8_t computeNode(int n, int x, int y, int z) {
     double c = (n-1)/2.;
     Vector3 pos = (Vector3){(double)x-c, (double)y-c, (double)z-c};
     double dist = sdf(pos, (Vector2){6., 3.})/(sqrt(2))*127;
-    uint32_t val = (uint32_t)(intClamp((int)dist, -127, 127)+127);
-    int material = 0;
-    uint32_t node = val | material<<8; // other bits are used for material storing
-    octtreeBuffer[index] = node;
+    uint8_t val = (uint8_t)(intClamp((int)dist, -127, 127)+127);
+	return val;
 }
 
-void updateBuffer(uint32_t* octtreeBuffer, int p, int n) {
+bool nodeEquality(Node* node1, Node* node2) {
+	return node1->isLeaf==node2->isLeaf
+		&& node1->children==node2->children
+		&& node1->cache==node2->cache
+		&& node1->data==node2->data;
+}
+
+Node* createOctree(int p, int n, int x, int y, int z) {
+	if(p==1) {
+		Node* node = malloc(sizeof(Node));
+		for(int i = 0; i<8; i++) {
+			node->children[i] = nullptr;
+		}
+		node->isLeaf = true;
+		node->cache=0;
+		node->data=computeNode(n, x, y, z);
+	}
+	else {
+		int offset = 1<<(p-1);
+		Node* node000 = createOctree(p-1, n, x, y, z);
+		Node* node100 = createOctree(p-1, n, x+offset, y, z);
+		Node* node010 = createOctree(p-1, n, x, y+offset, z);
+		Node* node110 = createOctree(p-1, n, x+offset, y+offset, z);
+		Node* node001 = createOctree(p-1, n, x, y, z+offset);
+		Node* node101 = createOctree(p-1, n, x+offset, y, z+offset);
+		Node* node011 = createOctree(p-1, n, x, y+offset, z+offset);
+		Node* node111 = createOctree(p-1, n, x+offset, y+offset, z+offset);
+		if (node000->isLeaf
+		&& nodeEquality(node000, node100)
+		&& nodeEquality(node000, node010)
+		&& nodeEquality(node000, node110)
+		&& nodeEquality(node000, node001)
+		&& nodeEquality(node000, node101)
+		&& nodeEquality(node000, node011)
+		&& nodeEquality(node000, node111)
+		) {
+			Node* node = node000;
+			free(node100);
+			free(node010);
+			free(node110);
+			free(node001);
+			free(node101);
+			free(node011);
+			free(node111);
+			return node;
+		}
+		else {
+			Node* node = malloc(sizeof(Node));
+			node->children[0] = node000;
+			node->children[1] = node100;
+			node->children[2] = node010;
+			node->children[3] = node110;
+			node->children[4] = node001;
+			node->children[5] = node101;
+			node->children[6] = node011;
+			node->children[7] = node111;
+			node->data=0;
+			node->isLeaf=false;
+			uint8_t cache = (node000->isLeaf ? true : false)
+				| (node100->isLeaf ? 0x1 : 0x0)<<1
+				| (node010->isLeaf ? 0x1 : 0x0)<<2
+				| (node110->isLeaf ? 0x1 : 0x0)<<3
+				| (node001->isLeaf ? 0x1 : 0x0)<<4
+				| (node101->isLeaf ? 0x1 : 0x0)<<5
+				| (node011->isLeaf ? 0x1 : 0x0)<<6
+				| (node111->isLeaf ? 0x1 : 0x0)<<7;
+			return node;
+		}
+	}
+}
+
+void updateBuffer(uint32_t* octreeBuffer, int p, int n) {
 }
 
 int main ()
@@ -96,7 +171,7 @@ int main ()
 
     int p = 5;
 	int n = 1<<p;
-    int32_t octtreeBuffer = malloc(quickExp(8, p)*sizeof(int32_t));
+    int32_t octreeBuffer = malloc(quickExp(8, p)*sizeof(int32_t));
 	
 	bool updateEnabled = false;
 	int mode = 0;
