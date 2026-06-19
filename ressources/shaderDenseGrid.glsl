f3e7dd04cfd786455e88f9ed4ba9061d8feedd22
TheRedTanuki <petitmars@outlook.com>
2026-06-19 19:06:24 +0200

    Reset color on dense grid

--- ressources/shaderDenseGrid.glsl
+++ ressources/shaderDenseGrid.glsl
@@ -283,7 +283,7 @@ void main() {
         float tNext = min(tMax.x, min(tMax.y, tMax.z));
         vec4 color = intersectVoxel(currentVoxel, position + t*ray, ray, tNext-t);
         if (color!=vec4(0.)) {
-            finalColor = vec4(vec3(1.)-color.xyz, 1.);
+            finalColor = color;
             return;
         }
         if(tMax.x < tMax.y) {
@@ -311,6 +311,6 @@ void main() {
             }
         }
     }
-    finalColor = vec4(1., 1., 1., 1.);
+    finalColor = vec4(0., 0., 0., 1.);
     return;
 }
